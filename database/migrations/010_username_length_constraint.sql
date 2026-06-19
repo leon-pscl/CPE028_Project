419e6715cf5517f12509d03f14c207f779fed59f
leon-pscl <leon.pscual@gmail.com>
2026-06-19 16:24:41 +0800

    fix: truncate long full_name before adding length constraint in 010

--- database/migrations/010_username_length_constraint.sql
+++ database/migrations/010_username_length_constraint.sql
@@ -1,3 +1,6 @@
+-- Truncate any full_name values that exceed 100 chars
+UPDATE public.users SET full_name = left(full_name, 100) WHERE char_length(full_name) > 100;
+
 -- Enforce max length on full_name to prevent abuse
 DO $$
 BEGIN
